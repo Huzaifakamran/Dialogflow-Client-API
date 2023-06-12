1cb95e048e25d91ae6dafcc237fb9d3b7de7d857
Huzaifa Kamran <huzaifa.2014@hotmail.com>
2023-06-12 05:00:32 +0500

    6/12/2023

--- app.py
+++ app.py
@@ -1,45 +1,262 @@
 from flask import Flask, request, jsonify,render_template
-from google.cloud import dialogflow
+# from google.cloud import dialogflow
 from flask_cors import CORS
+import requests
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+import json
+import os
+import openai
+import psycopg2
+from datetime import datetime
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/', methods=['GET'])
-def home():
-   return render_template('index.html')
+load_dotenv()
+
+def realtorScrap(address):
+    # print(address)
+    my_dict = {}
+    addressComponents = address['address']['address_components']
+    for record in addressComponents:
+        if 'street_number' in record['types']:
+            my_dict['street_number'] = record['short_name']
+            continue
+        if 'route' in record['types']:
+            my_dict['route'] = record['long_name']
+            continue
+        if 'locality' in record['types']:
+            my_dict['locality'] = record['short_name']
+            continue
+        if 'administrative_area_level_1' in record['types']:
+            my_dict['areaLevelOne'] = record['short_name']
+            break
+    # print(my_dict)
+    locality = my_dict['locality'].replace(' ','-')
+    print(locality)
+    areaLevelOne = my_dict['areaLevelOne']
+    print(areaLevelOne)
+    o={}
+    target_url = f"https://www.realtor.com/realestateandhomes-search/{locality}_{areaLevelOne}"
+    head={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"}
+    resp = requests.get(target_url, headers=head)
+    soup = BeautifulSoup(resp.text, 'html.parser')
+    allData = soup.find_all("div",{"class":"property-wrap"})
+    route = my_dict['route'].split(' ')[0]
+    print('route',route)
+    inputAddress = my_dict['street_number'] + ' ' + route
+    print('input',inputAddress)
+    # print(allData)
+    for i in range(0, len(allData)):
+        address = allData[i].find("div",{"data-label":"pc-address"}).text
+        # print(address)
+        if inputAddress in address:
+            print('under if')
+            o["price"]=allData[i].find("span",{"data-label":"pc-price"}).text
+            metaData = allData[i].find("ul",{"class":"property-meta"})
+            allMeta = metaData.find_all("li")
+            for x in range(0, len(allMeta)):
+                try:
+                    o["bed"]=allMeta[0].text
+                except:
+                    o["bed"]=None
+                try:
+                    o["bath"]=allMeta[1].text
+                except:
+                    o["bath"]=None
+                try:
+                    o["size-sqft"]=allMeta[2].text
+                except:
+                    o["size-sqft"]=None
+                try:
+                    o["size-acre"]=allMeta[3].text
+                except:
+                    o["size-acre"]=None
+            o["address"]=allData[i].find("div",{"data-label":"pc-address"}).text
+            o['status'] = True
+            # print(o)
+            return json.dumps(o)
+        
+    return json.dumps({
+                "status":False,
+                "message":"property details unavailable please manually enter property details"
+                })
+
+@app.route('/<id>', methods=['GET'])
+def home(id):
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    query1 = conn.cursor()
+    query1.execute("Select Property_address,Created_date from public.users where User_id = %s",(id,))
+    data = query1.fetchall()
+    query1.close()
+    print(data)
+    submissions = [list(item) for item in data]
+    return render_template('index.html',submissions=submissions)
 
 @app.route('/address', methods=['POST'])
 def address():
     address = request.json
-    print(address['address']['formatted_address'])
-    return 'received'
-
+    # formattedAddress = address['address']['formatted_address']
+    result = realtorScrap(address)
+    print(result)
+    return result
+    
 @app.route('/chatbot', methods=['POST'])
 def chatbot():
-    user_message = request.json['message']
-    print(user_message)
-    print(user_id)
-    user_id = request.args.get('user_id')
+    user_message = request.json
+    address = user_message['address']
+    price = user_message['price']
+    bed = user_message['bed']
+    bath = user_message['bath']
+    sizeSqft = user_message['sizeSqft']
+    status = request.args.get('status')
+    openai.api_key = os.getenv("OPEN_API_KEY")
+    if(status == '1'):
+        print('under status')
+        firstPrompt = f'''
+        act as a copywriter specializing in creating listing descriptions for homes for sale. 
+        Your objective is to create two unique an engaging and persuasive listing description 
+        free of grammatical errors for a property for sale. This description will be read by 
+        potential buyers of the property who want to read more and schedule a showing. 
+        Make this description no less than 750 characters and more than 1,500 characters. 
+        You must only use true and factual data. Highlight the location of the primary suite 
+        if there is one. Make the description flow as if the reader is walking room to room in the home. 
+        Use all of the following data no more than once to create the descriptions.
+        address = {address}\n
+        price = {price}\n
+        bed = {bed}\n
+        bath = {bath}\n
+        sizeSqft = {sizeSqft}
+        '''
+        response = openai.Completion.create(
+        model="text-davinci-003", 
+        prompt=firstPrompt, 
+        temperature=0, 
+        max_tokens=1500
+        )
+        firstResponse = response['choices'][0]['text']
+        # print(firstResponse)
+        print("GET FIRST RESPONSE")
+        return jsonify({
+        # "firstResponse":firstResponse
+        "firstResponse":firstResponse
+        # "secondResponse":secondResponse
+    })
+    else:
+        secondPrompt = f'''
+        Act as a social media marketer with an expertise in writing captions for homes for sale. 
+        Your objective is to create captions for a facebook post, instagram post, and twitter post using the data below. 
+        You must stay in the character limits of each application for each post. The goal is to create an engaging enough caption to 
+        entice a potential buyer to click on the link provided for more info. You must include a placeholder for the link to the property. 
+        You must include relevant emojis. You must include up to 9 hashtags that are relevant and heavily used in the real estate industry. 
+        Layout the caption as such:
+
+        [Headline - Example - Hot New Listing Alert ]
+        [Address - {address}]
+        [Price - {price}]
+        [Bedroom count - {bed}]
+        [bathroom count - {bath}]
+        [square footage - {sizeSqft}]
+
+        [closing lines, insert link to property, and hashtags]
+
+    '''
+        response = openai.Completion.create(
+        model="text-davinci-003", 
+        prompt=secondPrompt, 
+        temperature=0, 
+        max_tokens=1500
+        )
+        secondResponse = response['choices'][0]['text']
+        return jsonify({
+            "secondResponse":secondResponse
+        })
+
+    # return jsonify(response['choices'][0]['text'])
+    # print(user_id)
+    # user_id = request.args.get('user_id')
 
     # Send user message to Dialogflow and get response
-    response = detect_intent(user_id, user_message)
+    # response = detect_intent(user_id, user_message)
 
     # Return bot response
-    return jsonify({'message': response})
+    # return jsonify({'message': response})
 
-def detect_intent(user_id, user_message):
-    session_client = dialogflow.SessionsClient()
-    PROJECT_ID = 'hotel-book-2d76a'
-    session_id = f'projects/{PROJECT_ID}/agent/sessions/{user_id}'
-    session = session_client.session_path(PROJECT_ID, session_id)
+@app.route('/saveChat', methods =['POST'])
+def saveChat():
+    try:
+        print('reached here')
+        chatdata = request.json
+        status = request.args.get('status')
+        DATABASE_URL = os.getenv("DATABASE_URL")
+        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+        print('before if')
+        if(status == '1'):
+            print('under status 1')
+            formattedAddress = chatdata[0]['formattedAddress']
+            price = chatdata[0]['price']
+            bed = chatdata[0]['bed']
+            bath = chatdata[0]['bath']
+            sizeSqft = chatdata[0]['sizeSqft']
+            sizeAcre = chatdata[0]['sizeAcre']
+            propertyType = chatdata[0]['propertyType']
+            yearBuilt = chatdata[0]['yearBuilt']
+            architecturalStyle = chatdata[0]['architecturalStyle']
+            bedBathDist = chatdata[0]['bedBathDist']
+            commName = chatdata[0]['commName']
+            schoolDist = chatdata[0]['schoolDist']
+            keyFeatures = chatdata[0]['keyFeatures']
+            EEASF = chatdata[0]['EEASF']
+            HOA = chatdata[0]['HOA']
+            additionalComments = chatdata[0]['additionalComments']
+            print(additionalComments)
+            datetime_str = chatdata[0]['dateTime']
+            datetime_obj = datetime.strptime(datetime_str, '%m/%d/%Y, %I:%M:%S %p')
+            dateTime = datetime_obj.strftime('%Y-%m-%d %H:%M:%S.%f')
+            query1 = conn.cursor()
+            query1.execute("INSERT INTO public.users ( User_id, Created_date,Property_address, Property_type, Listing_price, Number_of_bedrooms, Number_of_bathrooms, Square_footage, Lot_size, Year_built, Architectural_style, Bed_bath_dist, Community_name, School_district, Key_features, Energy_efficiency, Hoa_info, Additional_comments) Values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) RETURNING id",(123,dateTime,formattedAddress,propertyType,price,bed,bath,sizeSqft,sizeAcre,yearBuilt,architecturalStyle,bedBathDist,commName,schoolDist,keyFeatures,EEASF,HOA,additionalComments))
+            users_id = query1.fetchone()[0]
+            query1.close()
+            for record in chatdata[1 : ]:
+                sender = record['sender']
+                message = record['message']
+                user_id = users_id
+                datetime_str = record['dateTime']
+                datetime_obj = datetime.strptime(datetime_str, '%m/%d/%Y, %I:%M:%S %p')
+                dateTime = datetime_obj.strftime('%Y-%m-%d %H:%M:%S.%f')
+                query2 = conn.cursor()
+                query2.execute("INSERT INTO public.chat_history (Message, User_id, Created_date,Sender) Values (%s,%s,%s,%s)",(message,user_id,dateTime,sender))
+                query2.close()
+            conn.commit()
+            return jsonify({
+                "status":"Data Saved Successfully"
+            })
+        elif status == '2':       
+            openai.api_key = os.getenv("OPEN_API_KEY")
+            botList = []
+            for rec in chatdata[1 : ]:
+                if rec['sender'] == 'bot':
+                    botList.append(rec['message'])
+            botResponse = '\n'.join(botList)
+            message = chatdata[-1]
+            custMessage = message['message']
+            response = openai.Completion.create(
+            model="text-davinci-003", 
+            prompt=f"{botResponse}\nAbove is the response please answer below question by considering the above response\n{custMessage}", 
+            temperature=0, 
+            max_tokens=1500
+            )
+            Response = response['choices'][0]['text']
+            return jsonify({
+                'response': Response
+            })
+    except Exception as e:
+        print(e)
+    finally:
+        conn.close()
 
-    text_input = dialogflow.types.TextInput(text=user_message, language_code='en-US')
-    query_input = dialogflow.types.QueryInput(text=text_input)
-
-    response = session_client.detect_intent(session=session, query_input=query_input)
-
-    return response.query_result.fulfillment_text
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/main.js
+++ static/main.js
@@ -4,14 +4,27 @@ const infoSection = document.querySelector('.info-section');
 const mapSection = document.querySelector('.map-section');
 const submitButton = document.querySelector('.submit-button');
 const chatMessages = document.querySelector('#chat-messages');
+const saveOutput = document.querySelector('#save-output');
+const saveOutputText = document.getElementById('save-output-text');
+const saveOutputLoading = document.getElementById('save-output-loading');
+const spinnerDiv = document.getElementById('Spinner');
+const chatInput = document.getElementById('chatInput');
+const submitBtn = document.getElementById('submitBtn');
+const viewDetails = document.getElementById('viewDetails');
+const dynamicContentElement = document.getElementById("dynamicContent");
+const chatHistory = [];
 
 //Autocomplete address field
  function initialize() {
-  console.log('reached111')
+  // console.log('reached111')
+  chatHistory.formattedAddress = '';
   const input = document.getElementById('fromloc');
   const autocomplete = new google.maps.places.Autocomplete(input);
   google.maps.event.addListener(autocomplete, 'place_changed', function () {
+    spinnerDiv.style.display = 'flex';
+    spinnerDiv.classList.remove("spinnerHidden"); // start spinning
     const place = autocomplete.getPlace();
+    // console.log(place)
     const data = {address:place};
       // document.getElementById('city2').value = place.name;
       // document.getElementById('cityLat').value = place.geometry.location.lat();
@@ -22,52 +35,228 @@ const chatMessages = document.querySelector('#chat-messages');
           'Content-Type': 'application/json'
         },
         body: JSON.stringify(data)
-      }).then(response =>{
-        console.log(response)
-      }).catch(error =>{
-        console.log("error: ",error);
       })
+      .then(response => response.json())
+      .then(data => {
+        // console.log(data)
+        if (data.status != true) {
+          spinnerDiv.classList.add('spinnerHidden');
+          // spinnerDiv.style.display = 'none';
+          alert(data.message)
+        }
+        else{
+          
+          spinnerDiv.classList.add('spinnerHidden'); 
+          const address = data.address;
+          const status = data.status;
+          const price = data.price;
+          const bed = data.bed;
+          const bath = data.bath;
+          const sizeSqft = data["size-sqft"];
+          const sizeAcre = data["size-acre"];
+
+          document.getElementById('field1').value = address;
+          document.getElementById('field3').value = price;
+          document.getElementById('field4').value = bed;
+          document.getElementById('field5').value = bath;
+          document.getElementById('field6').value = sizeSqft;
+          document.getElementById('field7').value = sizeAcre;
+        }
+      })
+      .catch(error => {
+        console.error("Error:", error);
+      });
+      
   });
 }
 google.maps.event.addDomListener(window, 'load', initialize);
-
-
-// Event listener for submit button click
 submitButton.addEventListener('click', async () => {
-  console.log('Clicked')
-  // Get user message
-  const userMessage = searchField.value;
+  const typingAnimation = document.getElementById('typing-animation');
+  const address = document.getElementById('field1').value;
+  const propertyType = document.getElementById('field2').value;
+  const price = document.getElementById('field3').value;
+  const bed = document.getElementById('field4').value;
+  const bath = document.getElementById('field5').value;
+  const sizeSqft = document.getElementById('field6').value;
+  const lotSize = document.getElementById('field7').value;
+  const keyFeatures = document.getElementById('field13').value;
+  const yearBuilt = document.getElementById('field8').value;
+  const architecturalStyle = document.getElementById('field9').value; 
+  const bedBathDist = document.getElementById('field10').value;
+  const commName = document.getElementById('field11').value;
+  const schoolDist = document.getElementById('field12').value;
+  const EEASF = document.getElementById('field14').value;
+  const HOA = document.getElementById('field15').value;
+  const additionalComments = document.getElementById('field16').value;
+  if (!address || !propertyType || !lotSize || !yearBuilt || !keyFeatures || !price || !bed || !bath || !sizeSqft) {
+    alert('Please fill out all mandatory fields');
+    
+  }
+  
+  else{
+  typingAnimation.style.display = 'block';
+  const currentDate = new Date();
+  const dateTime = currentDate.toLocaleString();
+  chatHistory.push({
+    formattedAddress: address,
+    price: price,
+    bed: bed,
+    bath: bath,
+    sizeSqft: sizeSqft,
+    sizeAcre: lotSize,
+    propertyType: propertyType,
+    yearBuilt: yearBuilt,
+    architecturalStyle: architecturalStyle,
+    bedBathDist: bedBathDist,
+    commName: commName,
+    schoolDist: schoolDist,
+    keyFeatures: keyFeatures,
+    EEASF: EEASF,
+    HOA: HOA,
+    additionalComments: additionalComments,
+    dateTime: dateTime
+  })
+  
+  const userData = {
+    'address':address,
+    'price':price,
+    'bed':bed,
+    'bath':bath,
+    'sizeSqft':sizeSqft,
+    'sizeAcre':lotSize
+  };
 
   // Clear search field
   searchField.value = '';
 
-  // Display user message
-  displayUserMessage(userMessage);
-  const USER_ID = '123'
-  // Send user message to server
-  const response = await fetch(`/chatbot?user_id=${USER_ID}`, {
+  // const response = await fetch(`/chatbot?user_id=${USER_ID}`, {
+  const response = await fetch(`/chatbot?status=1`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({ message: userMessage })
+    body: JSON.stringify(userData)
   });
-  // Get response from server
-  const data = await response.json();
 
-  // Display bot response
-  const botMessage = data.message;
-  displayBotMessage(botMessage);
+  const data = await response.json();
+  typingAnimation.style.display = 'none';
+  // console.log(data.firstResponse)
+  displayBotMessage(data.firstResponse);
+
+  // Second Response
+  typingAnimation.style.display = 'block';
+  const secondresponse = await fetch(`/chatbot?status=2`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(userData)
+  });
+
+  const secondData = await secondresponse.json();
+  typingAnimation.style.display = 'none';
+  displayBotMessage(secondData.secondResponse);
+  // displayBotMessage(data.secondResponse);
+  }
 });
+
+//Customer message
+function handleCustomerMessage() {
+  const message = chatInput.value;
+  displayUserMessage(message);
+  chatInput.value='';
+  const chatData = JSON.stringify(chatHistory);
+  fetch('/saveChat?status=2', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: chatData
+  })
+  .then(response => response.json())
+
+  .then(data => {
+    console.log(data)
+    displayBotMessage(data.response)
+  })
+  .catch(error => {
+    console.error('Error saving chat history:', error);
+  });
+}
+
+function handleCustomerMessageEvents(event) {
+  if (event.type === 'keypress' && event.key === 'Enter') {
+    handleCustomerMessage();
+  } else if (event.type === 'click') {
+    handleCustomerMessage();
+  }
+}
+chatInput.addEventListener('keypress', handleCustomerMessageEvents);
+submitBtn.addEventListener('click', handleCustomerMessageEvents);
+
+//Save Output
+saveOutput.addEventListener('click', async () =>{
+  console.log(chatHistory)
+  console.log('clicked')
+
+  // Show loading sign and disable the button
+  saveOutputText.style.display = 'none';
+  saveOutputLoading.style.display = 'inline-block';
+  saveOutput.disabled = true;
+
+  const chatData = JSON.stringify(chatHistory);
+  fetch('/saveChat?status=1', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: chatData
+  })
+  .then(response => response.json())
+
+  .then(data => {
+    saveOutputText.style.display = 'inline-block';
+    saveOutputLoading.style.display = 'none';
+    saveOutput.disabled = false;
+    alert(data.status);
+  })
+  .catch(error => {
+    console.error('Error saving chat history:', error);
+  });
+})
 
 // Function to display bot message
 function displayBotMessage(message) {
+  const container = document.createElement('div');
+  container.classList.add('message-container');
   const messageElement = document.createElement('div');
   messageElement.classList.add('message', 'bot');
+  messageElement.style.borderRadius = '15px';
   const messageText = document.createElement('p');
   messageText.innerText = message;
   messageElement.appendChild(messageText);
-  chatMessages.appendChild(messageElement);
+  container.appendChild(messageElement);
+
+  const copyButton = document.createElement('button');
+  copyButton.innerText = 'Copy';
+  copyButton.classList.add('reqbutton');
+  copyButton.addEventListener('click', () => {
+    copyToClipboard(message);
+  });
+  container.appendChild(copyButton);
+
+  const conversation = document.querySelector('.conversation');
+  conversation.appendChild(container);
+
+  const currentDate = new Date();
+  const dateTime = currentDate.toLocaleString();
+  chatHistory.push({
+    sender: 'bot',
+    message: message,
+    dateTime: dateTime
+  });
+  // chatMessages.insertBefore(messageElement, chatMessages.firstChild);
+  // chatMessages.appendChild(messageElement);
 }
 
 // Function to display user message
@@ -78,4 +267,35 @@ function displayUserMessage(message) {
   messageText.innerText = message;
   messageElement.appendChild(messageText);
   chatMessages.appendChild(messageElement);
+  
+  const conversation = document.querySelector('.conversation');
+  conversation.appendChild(messageElement);
+
+  const currentDate = new Date();
+  const dateTime = currentDate.toLocaleString();
+  chatHistory.push({
+    sender: 'user',
+    message: message,
+    dateTime: dateTime
+  });
 }
+
+function copyToClipboard(text) {
+  const textarea = document.createElement('textarea');
+  textarea.value = text;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand('copy');
+  document.body.removeChild(textarea);
+  alert('Chat content copied to clipboard!');
+}
+
+viewDetails.addEventListener('click', async () => {
+// Iterate over the submissions array and generate the HTML dynamically
+submissions.forEach(submission => {
+  var submissionDiv = document.createElement("div");
+  submissionDiv.classList.add("hisor", "rounded");
+  submissionDiv.textContent = submission;
+  dynamicContentElement.appendChild(submissionDiv);
+  });
+})
